d1ce4f8e4f9ee63ad1f477770148655454e662e5
Martin Lui <devel@martian111.net>
2018-08-14 02:37:58 -0700

    Moved code setting initial value to observe method
    Before the change, the initial value of the "Bindable" object is set
    only by the Bindable.bind() method.  This change moved that initial set
    value code to the NSObject.observe() method in the NSObject extension.
    The Bindable.bind() method calls the NSObject.observe() method within
    it.  As a result, the initial value is set regardless of which method is
    called.

--- Sources/Bindable.swift
+++ Sources/Bindable.swift
@@ -58,12 +58,9 @@ extension Bindable where Self: NSObject {
             _self.addTarget(Selector, action: Selector{ self.valueChanged() }, for: [.editingChanged, .valueChanged])
         }
         self.binder = observable
-        if let val = observable.value {
-            self.updateValue(with: val)
-        }
         self.observe(for: observable) { (value) in
             self.updateValue(with: value)
         }
     }
-    
+
 }
